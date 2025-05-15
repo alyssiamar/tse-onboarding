e9af836ea26ae00da4ed3c09e90c0ceef05bb8ad
alyssiamar <118578524+alyssiamar@users.noreply.github.com>
2025-05-14 17:30:20 -0700

    fixing more pull request errors

--- backend/src/controllers/tasks.ts
+++ backend/src/controllers/tasks.ts
@@ -1,5 +1,5 @@
 import { RequestHandler } from "express";
-import task from "src/models/task";
+//import task from "src/models/task";
 import TaskModel from "src/models/task";
 
 export const getAllTasks: RequestHandler = async (req, res, next) => {
